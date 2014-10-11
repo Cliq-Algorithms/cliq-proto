475b387a11079b49664cbff063b4380555af3bea
Shekar Brahma <bshekar96@gmail.com>
2014-10-11 18:56:43 -0500

    tag search done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 module ApplicationHelper
-  
+
   # Return a title on a per-page basis.
+  def tag_search
+    @tag = "sports"
+    @eventsCloseBy = events.search
+    @eventsWithTag = Array[]
+    eventsCloseBy.each do |eventsCloseBy|
+      if(eventsCloseBy.include? tag)
+        eventsWithTag.push(eventsCloseBy)
+    end
+  
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
@@ -9,7 +18,7 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
-  
+
   def logo
     image_tag("logo.png", :alt => "Sample App", :class => "round")
   end
