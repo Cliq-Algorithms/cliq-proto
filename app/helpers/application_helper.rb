3af2dac7fee65fb1f2b1f7f6a23cac56f141e252
Shekar Brahma <bshekar96@gmail.com>
2014-10-11 19:32:09 -0500

    partial search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,24 @@
 module ApplicationHelper
 
   # Return a title on a per-page basis.
-  def tag_search
+  def tag_search?
     @tag = "sports"
     @eventsCloseBy = events.search
     @eventsWithTag = Array[]
     eventsCloseBy.each do |eventsCloseBy|
       if(eventsCloseBy.include? tag)
         eventsWithTag.push(eventsCloseBy)
+      end
     end
-  
+  end
+
+  def event_search?
+      @events = Event.all
+      @withinRadius = @current_user.radius > (((@current_user.lat - @event.lat)**2 +(@current_user.lon - @event.lon)**2)**.5)
+      @local_event = @event.where(withinRadius => true)
+    end
+  end
+      
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
